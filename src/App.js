061a68d6647b9fbf54df5819fc6e80c402f046b0
MC Villoso <mcvilloso@gmail.com>
2022-11-17 14:57:03 -0800

    Update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Pages/home';
+import Contact from './Pages/contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+
+    <Header/>
+    <Routes>
+        <Route exact path='/'  element={<Home />} />
+        <Route path='/contact' element={<Contact/>} />
+    </Routes>    <Footer/>
+          </Router>
+          </>
   );
 }
 
---
+++ src/Pages/project.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProjectCard(){
+    return (
+        <div>Project</div>
+    )
+}
+
+export default ProjectCard
